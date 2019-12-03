cae4a004d01cd15d3eed7712c9880d29b9d35842
careful-principle-0303 <cabaker96@aol.com>
2019-12-03 21:19:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,8 @@ def square_array(array)
   counter = 0
   squared_array = []
   while(counter < array.length) do
-    squared_array.push(array[counter]**2)
+    newnum = array[counter]**2
+    squared_array.push(newnum)
     counter += 1 
   end
 end
